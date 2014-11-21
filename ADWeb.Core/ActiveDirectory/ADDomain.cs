6e5139d09ba310f35bc198e740b2a1f77068cbd5
Ed Panameno <ed.panameno@gmail.com>
2014-11-20 23:06:21 -0800

    Issue #19 - I started to add more fields to search for in the quick search ... but at the time of this commit this is not working. I may need to revisit this later as I did a search for the notes on the object and I could not get anything back that I though should have resulted in some users being retrieved.

--- ADWeb.Core/ActiveDirectory/ADDomain.cs
+++ ADWeb.Core/ActiveDirectory/ADDomain.cs
@@ -224,7 +224,11 @@ namespace ADWeb.Core.ActiveDirectory
             {
                 ADUser userFilter = new ADUser(context)
                 {
-                    DisplayName = "*" + searchString + "*"
+                    DisplayName = "*" + searchString + "*",
+                    SamAccountName = "*" + searchString + "*",
+                    EmailAddress = "*" + searchString + "*",
+                    Description = "*" + searchString + "*",
+                    Notes = "*" + searchString + "*"
                 };
 
                 using(PrincipalSearcher searcher = new PrincipalSearcher(userFilter))
