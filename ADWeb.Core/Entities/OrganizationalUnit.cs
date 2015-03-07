fcd188c1b406279aa01685f5cb1877e8a3a5743c
Ed Panameno <ed.panameno@gmail.com>
2015-03-07 13:48:17 -0800

    Added a new class that will be used to store information about Organizational Units. I also made some minor changes to the the UI of the Users section of the application.

---
+++ ADWeb.Core/Entities/OrganizationalUnit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADWeb.Core.Entities
+{
+    public class OrganizationalUnit
+    {
+        public int OrganizationalUnitID { get; set; }
+        public string Path { get; set; }
+        public string Name { get; set; }
+        public string Notes { get; set; }
+    }
+}
