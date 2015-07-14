cf10eee4b559f8f29eeab309e03bdb4086fa0371
Ed Panameno <ed.panameno@gmail.com>
2015-07-13 18:40:15 -0700

    Removed commented out code .. I am still not happy that I have to use this to view and edit user templates, but the alternative is to use another view model class (this too is something I am not too crazy over). But the alternative is to make the UserTemplateVM more complicated then it is ... which I want to avoid like the plauge!
    I may revisit this topic but for now, I am happy that I can view and
    edit a user template.

--- ADWeb/ViewModels/ViewUserTemplateVM.cs
+++ ADWeb/ViewModels/ViewUserTemplateVM.cs
@@ -18,24 +18,6 @@ namespace ADWeb.ViewModels
             Groups = new List<string>();
         }
 
-        /*[Remote("IsTemplateNameUnique", "Admin", ErrorMessage="Template Name is not unique. Please enter a unique Template name.")]
-        public string Name { get; set; }
-        public int DomainOUID { get; set; }
-        
-        public bool ChangePasswordAtNextLogon { get; set; }
-        public bool UserCannotChangePassword { get; set; }
-        public bool PasswordNeverExpires { get; set; }
-        public bool AccountExpires { get; set; }
-        
-        public UserExpirationRange? ExpirationRange { get; set; }
-        public int? ExpirationValue { get; set; }
-        
-        [AllowHtml]
-        public string Notes { get; set; }
-
-        public List<DomainOU> OrganizationalUnits { get; set; }
-        public List<string> Groups { get; set; }*/
-
         public UserTemplate UserTemplate { get; set; }
         public List<DomainOU> OrganizationalUnits { get; set; }
         public List<string> Groups { get; set; }
